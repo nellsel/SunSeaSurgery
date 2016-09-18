caaa709b0f4066c5a12e127c6389121e9de61fc9
Myles <Myles@MYLES-PC>
2016-09-18 12:14:21 +0100

    Add project files.

---
+++ SunSeaSurgery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SunSeaSurgery.MvcApplication" Language="C#" %>
---
+++ SunSeaSurgery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SunSeaSurgery.Startup))]
+namespace SunSeaSurgery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
